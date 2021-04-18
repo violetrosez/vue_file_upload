ee00592818962cecc5ddb8b216ec8c7c2ff0f477
violetrosez <violetrosezq@gmail.com>
2021-04-18 19:58:18 +0800

    add

---
+++ src/components/Upload_v1.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="content">
+    <!-- element -->
+    <!-- <el-upload
+      class="upload-demo"
+      action="http://localhost:3000/upload"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :before-remove="beforeRemove"
+      multiple
+      :limit="3"
+      :on-exceed="handleExceed"
+      :file-list="fileList"
+    >
+      <el-button size="small" type="primary">点击上传</el-button>
+     
+    </el-upload> -->
+
+    <!-- v2 -->
+    <!-- <form action="http://localhost:3000/upload" method="POST" enctype="multipart/form-data"> 
+      <input type="file" name="file" id="file"/>
+      <input type="submit" value="提交">
+    </form> -->
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      fileList: [],
+    };
+  },
+  methods: {
+
+    handleUpload() {
+      let file = document.getElementById('file').files[0];
+      console.log(file);
+    },
+
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
+    handleExceed(files, fileList) {
+      this.$message.warning(
+        `当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${
+          files.length + fileList.length
+        } 个文件`
+      );
+    },
+    beforeRemove(file, fileList) {
+      console.log(fileList);
+      return this.$confirm(`确定移除 ${file.name}？`);
+    },
+  },
+};
+</script>
+
+
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.content {
+  width: 400px;
+  height: 400px;
+  border: 1px dashed #ccc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Upload_v2.vue
@@ -0,0 +1,108 @@
+<template>
+  <div style="width: 400px">
+    <div class="content" ref="drop">
+      <i class="el-icon-upload" style="font-size: 26px"></i>
+      拖拽文件到此处上传
+    </div>
+    <el-progress :text-inside="true" :stroke-width="16" :percentage="progress"></el-progress>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      progress: 0,
+    };
+  },
+  mounted() {
+    const dropbox = this.$refs.drop;
+
+    //监听拖拽事件
+
+    dropbox.addEventListener(
+      "dragleave",
+      () => {
+        dropbox.style.backgroundColor = "transparent";
+      },
+      false
+    );
+    dropbox.addEventListener(
+      "dragenter",
+      (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+      },
+      false
+    );
+    dropbox.addEventListener(
+      "dragover",
+      (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        dropbox.style.backgroundColor = "rgba(64,158,255,0.8)";
+      },
+      false
+    );
+    dropbox.addEventListener(
+      "drop",
+      (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        //获取文件
+        let file = e.dataTransfer.files[0];
+        //处理上传逻辑
+        this.handleUpload(file);
+        dropbox.style.backgroundColor = "transparent";
+      },
+      false
+    );
+  },
+  methods: {
+    /**
+     * 上传
+     */
+    handleUpload(file) {
+      let formData = new FormData();
+      formData.append("file", file);
+
+      let xhr = new XMLHttpRequest();
+
+      //监听进度
+      xhr.upload.onprogress = (e) => {
+        this.progress = parseInt(String((e.loaded / e.total) * 100));
+      };
+
+      xhr.open("post", "http://localhost:3000/upload");
+
+      xhr.send(formData);
+
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          console.log(xhr.responseText);
+          this.$message({
+            message: "上传成功",
+            type: "success",
+          });
+        }
+      };
+    },
+  },
+};
+</script>
+
+
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.content {
+  width: 400px;
+  height: 400px;
+  border: 1px dashed #ccc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 30px;
+}
+</style>
